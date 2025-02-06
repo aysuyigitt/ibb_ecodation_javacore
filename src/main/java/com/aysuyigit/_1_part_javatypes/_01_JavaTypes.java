b0d34513a2471c8f7ffdd26cc3c2e9bc72029797
aysuyigitt <79106302+aysuyigitt@users.noreply.github.com>
2025-02-06 19:08:41 +0300

    primitive

--- src/main/java/com/aysuyigit/_1_part_javatypes/_01_JavaTypes.java
+++ src/main/java/com/aysuyigit/_1_part_javatypes/_01_JavaTypes.java
@@ -1,7 +1,12 @@
 package com.aysuyigit._1_part_javatypes;
 
+//single comment
 public class _01_JavaTypes {
 
-
-
+    //psvm => tab
+    public static void main(String[] args) {
+        //Çıktı
+        //sout =>tab
+        System.out.println("Aysu Yiğit");
+    }
 }
---
+++ src/main/java/com/aysuyigit/_1_part_javatypes/_02_JavaPrimitiveTypes.java
@@ -0,0 +1,43 @@
+package com.aysuyigit._1_part_javatypes;
+
+public class _02_JavaPrimitiveTypes {
+
+    public static void main(String[] args) {
+
+        //Primitive Tpes: 8 tanedir. null değer alamaz.
+        //tam sayılar(4)
+        byte b1=125;
+        System.out.println("byte:" +b1);
+
+        short s1= 3267;
+        System.out.println("short:" +s1);
+
+        int i1= 253687155;
+        System.out.println("int:" +i1);
+
+        long l1= 989653257456L;
+        System.out.println("long:" +l1);
+
+        //virgüllü sayılar(2)
+        float f1= 3.14f;
+        System.out.println("float:" +f1);
+
+        double d1= 2.8;
+        System.out.println("double:" +d1);
+
+        //boolean
+
+        boolean b3= true;
+        System.out.println("boolean:" +b3);
+
+        //char
+        char c1= 'K';
+        System.out.println("char:" +c1);
+
+
+
+
+
+
+    }
+}
